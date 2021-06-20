542f2f5ed82a108d6498ce4c675a0683bf67eb6a
carlinjshaw <carlinjshaw@gmail.com>
2021-06-19 22:34:00 -0400

    inquirer set up; console log first two entries

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      message: 'Provide a description of your project',
+      name: 'description',
+    },
+  ])
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+.then(function(data){
+  console.log(data.title)
+})
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
